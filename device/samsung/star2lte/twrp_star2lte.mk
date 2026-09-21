e19d8cf91a4dc0b1880a2a948982d25f299bb1d0
Mehran <mehran2galaxy@gmail.com>
2026-09-21 16:29:07 +0330

    fix: inherit aosp_base.mk (embedded.mk gone in AOSP 12.1) + 2nd arch + ALLOW_MISSING_DEPENDENCIES
    official TWRP 12.1 tree pattern (a52q): device.mk -> vendor/twrp/common ->
    aosp_base.mk; lunch -eng per minimal-manifest README

--- device/samsung/star2lte/twrp_star2lte.mk
+++ device/samsung/star2lte/twrp_star2lte.mk
@@ -1,5 +1,6 @@
-$(call inherit-product, $(SRC_TARGET_DIR)/product/embedded.mk)
+$(call inherit-product, device/samsung/star2lte/device.mk)
 $(call inherit-product, vendor/twrp/config/common.mk)
+$(call inherit-product, $(SRC_TARGET_DIR)/product/aosp_base.mk)
 
 PRODUCT_DEVICE := star2lte
 PRODUCT_NAME := twrp_star2lte
